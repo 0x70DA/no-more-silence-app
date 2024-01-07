807fe1c670b528785e180b06c881a45d7ee65c87
Mahmoud <mahmoudh3257@gmail.com>
2024-01-07 19:59:31 +0200

    Handle login/sign up

--- App.jsx
+++ App.jsx
@@ -1,18 +1,65 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useEffect, useState} from 'react';
 import Welcome from './src/screens/Welcome';
 import Note from './src/screens/Note';
 import Login from './src/screens/Login';
+import List from './src/screens/List';
+import Details from './src/screens/Details';
+import {onAuthStateChanged} from 'firebase/auth';
+import {FIREBASE_AUTH} from './FirebaseConfig';
 
 const stack = createNativeStackNavigator();
 
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="My todos" component={List} />
+      <InsideStack.Screen name="details" component={Details} />
+    </InsideStack.Navigator>
+  );
+}
+
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, user => {
+      console.log('user', user);
+      setUser(user);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <stack.Navigator initialRouteName="Welcome">
-        <stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-        <stack.Screen name="Note" component={Note} options={{ headerShown: false }} />
-        <stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        {user ? (
+          <stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{headerShown: false}}
+          />
+        ) : (
+          <stack.Group initialRouteName="Welcome">
+            <stack.Screen
+              name="Welcome"
+              component={Welcome}
+              options={{headerShown: false}}
+            />
+            <stack.Screen
+              name="Note"
+              component={Note}
+              options={{headerShown: false}}
+            />
+            <stack.Screen
+              name="Login"
+              component={Login}
+              options={{headerShown: false}}
+            />
+          </stack.Group>
+        )}
       </stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,12 +1,263 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {FIREBASE_AUTH} from '../../FirebaseConfig';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 
 const Login = () => {
-    return (
-        <View>
-            <Text>Login Screen</Text>
-        </View>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+      alert('Login Failed: ' + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      console.log(response);
+      alert('Sign Up Success!');
+    } catch (err) {
+      console.log(err);
+      alert('Sign Up Failed: ' + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <ScrollView>
+          <View style={styles.backgroundContainer}>
+            <View style={styles.blueRectangle} />
+
+            <View style={styles.signUpTextContainer}>
+              <Text style={styles.signUpText}>Login/Sign Up</Text>
+            </View>
+            {/* TODO */}
+            <TouchableOpacity style={styles.googleButton}>
+              <Text style={styles.buttonText}>continue with google</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.facebookButton}>
+              <Text style={styles.buttonText}>continue with facebook</Text>
+            </TouchableOpacity>
+            {/* TODO */}
+            <View style={styles.orContainer}>
+              <Text style={styles.orText}>or</Text>
+            </View>
+
+            <View style={styles.emailTextContainer}>
+              <Text style={styles.emailText}>Email</Text>
+              <TextInput
+                value={email}
+                style={styles.emailInput}
+                autoCapitalize="none"
+                onChangeText={text => setEmail(text)}></TextInput>
+            </View>
+
+            <View style={styles.passwordTextContainer}>
+              <Text style={styles.passwordText}>Password</Text>
+              <TextInput
+                value={password}
+                style={styles.passwordInput}
+                autoCapitalize="none"
+                secureTextEntry={true}
+                onChangeText={text => setPassword(text)}></TextInput>
+            </View>
+
+            {loading ? (
+                <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+            <View>
+              <TouchableOpacity style={styles.signUpButton} onPress={signUp}>
+                <Text style={styles.buttonText}>sign up</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.loginButton} onPress={signIn}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+            )}
+
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundContainer: {
+    width: 429,
+    height: 926,
+    backgroundColor: 'white',
+    position: 'relative',
+  },
+  blueRectangle: {
+    position: 'absolute',
+    left: 17,
+    top: 138,
+    width: 381,
+    height: 592,
+    backgroundColor: '#E3EFFA',
+  },
+  signUpTextContainer: {
+    position: 'absolute',
+    left: 61,
+    top: 184,
+  },
+  signUpText: {
+    color: 'black',
+    fontSize: 24,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+  googleButton: {
+    position: 'absolute',
+    left: 33,
+    top: 276,
+    width: 329,
+    height: 38,
+    backgroundColor: '#F97B7B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  facebookButton: {
+    position: 'absolute',
+    left: 33,
+    top: 337,
+    width: 329,
+    height: 38,
+    backgroundColor: '#429AF2',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+  orContainer: {
+    position: 'absolute',
+    left: 196,
+    top: 421,
+  },
+  orText: {
+    color: 'black',
+    fontSize: 24,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+  emailTextContainer: {
+    position: 'absolute',
+    left: 33,
+    top: 463,
+  },
+  emailText: {
+    color: 'black',
+    fontSize: 24,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+  passwordTextContainer: {
+    position: 'absolute',
+    left: 33,
+    top: 550,
+  },
+  passwordText: {
+    color: 'black',
+    fontSize: 24,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+  emailInput: {
+    position: 'absolute',
+    left: 9,
+    top: 38,
+    width: 303,
+    height: 40,
+    backgroundColor: '#fff',
+  },
+  passwordInput: {
+    position: 'absolute',
+    left: 9,
+    top: 38,
+    width: 303,
+    height: 40,
+    backgroundColor: 'white',
+  },
+  loginButton: {
+    position: 'absolute',
+    left: 110,
+    top: 654,
+    width: 120,
+    height: 46,
+    backgroundColor: '#052E45',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signUpButton: {
+    position: 'absolute',
+    left: 242,
+    top: 654,
+    width: 120,
+    height: 46,
+    backgroundColor: '#052E45',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontFamily: 'Inter',
+    fontWeight: '800',
+    lineHeight: 30,
+  },
+});
 
 export default Login;
