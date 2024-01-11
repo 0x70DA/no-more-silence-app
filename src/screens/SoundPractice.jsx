b6c56bfd9e998d22ceb33e11b99e1e70228c2894
Mahmoud <mahmoudh3257@gmail.com>
2024-01-11 22:29:56 +0200

    Move `SoundsScreen` component to a seperate file

--- src/screens/SoundPractice.jsx
+++ src/screens/SoundPractice.jsx
@@ -1,83 +1,17 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-} from 'react-native';
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import PlaySoundScreen from './components/PlaySoundScreen';
+import SoundsScreen from './components/SoundsScreen';
 import SubSoundsScreen from './components/SubSoundsScreen';
+import PlaySoundScreen from './components/PlaySoundScreen';
 
 const SoundPracticeStack = createNativeStackNavigator();
-const sounds = [
-  'animal_and_insects_sounds',
-  'electric_devices_sounds',
-  'human_sounds',
-  'nature_sounds',
-  'public_sounds',
-  'public_transport_sounds',
-];
-
-
-const Sounds = ({ navigation }) => {
-  const [selectedSound, setSelectedSound] = useState(null);
-
-  const handleSoundPress = sound => {
-    setSelectedSound(sound);
-    navigation.navigate('SubSounds', { sound });
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.topBar} />
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.accountButton}
-          onPress={() => navigation.navigate('Account')}>
-          <Image
-            source={require('../../assets/account.png')}
-            style={styles.button}
-          />
-        </TouchableOpacity>
-
-        <View style={styles.screenTitle}>
-          <Text style={styles.screenTitleText}>Sounds</Text>
-        </View>
-
-        <TouchableOpacity
-          style={styles.homeButton}
-          onPress={() => navigation.navigate('Home')}>
-          <Image
-            source={require('../../assets/home.png')}
-            style={styles.button}
-          />
-        </TouchableOpacity>
-      </View>
-      {sounds.map((sound, index) => (
-        <TouchableOpacity
-          key={index}
-          style={styles.soundButton}
-          onPress={() => handleSoundPress(sound)}>
-          <Text style={styles.soundText}>
-            {sound.split('_').map(word => {
-              return word[0].toUpperCase() + word.slice(1);
-            })
-              .join(' ')}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-};
 
 const SoundPractice = () => {
   return (
-    <SoundPracticeStack.Navigator initialRouteName="Sounds">
+    <SoundPracticeStack.Navigator initialRouteName="SoundsScreen">
       <SoundPracticeStack.Screen
-        name="Sounds"
-        component={Sounds}
+        name="SoundsScreen"
+        component={SoundsScreen}
         options={{ headerShown: false }}
       />
       <SoundPracticeStack.Screen
@@ -93,86 +27,5 @@ const SoundPractice = () => {
     </SoundPracticeStack.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-  },
-  topBar: {
-    width: 413,
-    height: 72,
-    backgroundColor: '#052E45',
-  },
-  buttonContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    flexDirection: 'row',
-  },
-  button: {
-    width: 55,
-    height: 45,
-    resizeMode: 'contain',
-  },
-  accountButton: {
-    backgroundColor: '#D9D9D9',
-    left: 10,
-    top: 15,
-  },
-  screenTitle: {
-    position: 'relative',
-    width: 300,
-    top: 20,
-  },
-  screenTitleText: {
-    color: 'white',
-    fontSize: 21,
-    fontWeight: 'bold',
-    alignSelf: 'center'
-  },
-  homeButton: {
-    backgroundColor: '#D9D9D9',
-    right: 10,
-    top: 15,
-  },
-  soundButton: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#D9D9D9',
-    width: 351,
-    height: 73,
-  },
-  soundText: {
-    color: 'black',
-    fontSize: 24,
-    fontFamily: 'Inter',
-    fontStyle: 'italic',
-    fontWeight: '800',
-    textAlign: 'center',
-    padding: 10,
-  },
-  loadingText: {
-    padding: 5,
-    marginLeft: 10,
-    color: 'black'
-  },
-  subSoundButton: {
-    padding: 10,
-    marginVertical: 5,
-    backgroundColor: '#D9D9D9',
-    width: 351,
-    height: 73,
-  },
-  searchBar: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 2,
-    padding: 10,
-    marginTop: 10,
-    width: '90%',
-  },
-});
 
 export default SoundPractice;
---
+++ src/screens/components/SoundsScreen.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, } from 'react-native';
+
+const sounds = [
+  'animal_and_insects_sounds',
+  'electric_devices_sounds',
+  'human_sounds',
+  'nature_sounds',
+  'public_sounds',
+  'public_transport_sounds',
+];
+
+const SoundsScreen = ({ navigation }) => {
+  const [selectedSound, setSelectedSound] = useState(null);
+
+  const handleSoundPress = sound => {
+    setSelectedSound(sound);
+    navigation.navigate('SubSounds', { sound });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.accountButton}
+          onPress={() => navigation.navigate('Account')}>
+          <Image
+            source={require('../../../assets/account.png')}
+            style={styles.button}
+          />
+        </TouchableOpacity>
+
+        <View style={styles.screenTitle}>
+          <Text style={styles.screenTitleText}>Sounds</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.homeButton}
+          onPress={() => navigation.navigate('Home')}>
+          <Image
+            source={require('../../../assets/home.png')}
+            style={styles.button}
+          />
+        </TouchableOpacity>
+      </View>
+      {sounds.map((sound, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.soundButton}
+          onPress={() => handleSoundPress(sound)}>
+          <Text style={styles.soundText}>
+            {sound.split('_').map(word => {
+              return word[0].toUpperCase() + word.slice(1);
+            })
+              .join(' ')}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  topBar: {
+    width: 413,
+    height: 72,
+    backgroundColor: '#052E45',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 55,
+    height: 45,
+    resizeMode: 'contain',
+  },
+  accountButton: {
+    backgroundColor: '#D9D9D9',
+    left: 10,
+    top: 15,
+  },
+  screenTitle: {
+    position: 'relative',
+    width: 300,
+    top: 20,
+  },
+  screenTitleText: {
+    color: 'white',
+    fontSize: 21,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+  homeButton: {
+    backgroundColor: '#D9D9D9',
+    right: 10,
+    top: 15,
+  },
+  soundButton: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#D9D9D9',
+    width: 351,
+    height: 73,
+  },
+  soundText: {
+    color: 'black',
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontStyle: 'italic',
+    fontWeight: '800',
+    textAlign: 'center',
+    padding: 10,
+  },
+});
+
+export default SoundsScreen;
